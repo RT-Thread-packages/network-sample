e4175bfadd1ac557ac18f1770c4f16987e42301a
luo jiao <luojiaoxy@163.com>
2018-08-24 14:19:24 +0800

    add license information

--- tcpserver/tcpserver_sample.c
+++ tcpserver/tcpserver_sample.c
@@ -1,3 +1,12 @@
+/* 
+ * Copyright (c) 2006-2018, RT-Thread Development Team 
+ * 
+ * SPDX-License-Identifier: Apache-2.0 
+ * 
+ * Change Logs: 
+ * Date             Author      Notes
+ * 
+ */
 /*
 * 程序清单：tcp 服务端
  *
